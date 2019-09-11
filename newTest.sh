aa736fdd9f09889da189089638d994c039cb2974
binghuan li <binghuan.li@cern.ch>
2019-09-12 02:12:41 +0800

    update 2018 train config

--- newTest.sh
+++ newTest.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-bin/ttH/ttH_generic.x -config config/overall/ttHRunII/2016/ttH.MultiLeptons.DiLepton.All.config -inlist config/files/ttH_test/ttH_2016.list -hfile hists/SingleTop.105200_1.t.LP2fb_v5.MC.mm1+j.muonMSSmeardown.WtDilepton.root -skimfile skims/ttHDiLep_2016.root -mc -BkgdTreeName DiElectronPreTagTree  -UseTotalEvtFromFile -MCatNLO -mc -SelectTrigger TTHLep_2L -lepSFs -bTagReshape -PileUpWgt -chargeMis -Prefire -FakeRate -TriggerSFs -mcPromptFS -mcRightCharge 2>errorLog 
+#bin/ttH/ttH_generic.x -config config/overall/ttHRunII/2016/ttH.MultiLeptons.DiLepton.All.config -inlist config/files/ttH_test/ttH_2016.list -hfile hists/SingleTop.105200_1.t.LP2fb_v5.MC.mm1+j.muonMSSmeardown.WtDilepton.root -skimfile skims/ttHDiLep_2016.root -mc -BkgdTreeName DiElectronPreTagTree  -UseTotalEvtFromFile -MCatNLO -mc -SelectTrigger TTHLep_2L -lepSFs -bTagReshape -PileUpWgt -chargeMis -Prefire -FakeRate -TriggerSFs -mcPromptFS -mcRightCharge 2>errorLog 
+
+bin/ttH/ttH_generic.x -config config/overall/ttHRunII/2016/ttH.MultiLeptons.DiLepton.TrainMVA.config -inlist config/files/ttH_test/ttH_2016.list -hfile hists/SingleTop.105200_1.t.LP2fb_v5.MC.mm1+j.muonMSSmeardown.WtDilepton.root -skimfile skims/ttHDiLep_2016.root -mc -BkgdTreeName DiElectronPreTagTree  -UseTotalEvtFromFile -MCatNLO -mc -SelectTrigger TTHLep_2L -bTagReshape -Prefire -isTrainMVA 2>errorLog 
 
